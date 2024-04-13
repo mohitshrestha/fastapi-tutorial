20e0ae800d26d8b3f6ac2c633cce595877bebd83
Mohit Shrestha <mohitshrestha@gmail.com>
2024-04-13 02:58:11 -0700

    You can declare path "parameters" or "variables" with the same syntax used by Python format strings

--- 2-path-params/src/main.py
+++ 2-path-params/src/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
