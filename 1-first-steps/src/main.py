55f645dd382e73f29f60498085f7ebdd0ea77955
Mohit Shrestha <mohitshrestha@gmail.com>
2024-04-13 02:45:21 -0700

    Files for Branch 1-first-steps

---
+++ 1-first-steps/src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
