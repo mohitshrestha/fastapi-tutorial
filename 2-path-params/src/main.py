e60178f31ad71d24448d579d64cffa79a24d75e2
Mohit Shrestha <mohitshrestha@gmail.com>
2024-04-13 02:53:11 -0700

    Files for Branch 2-path-params

---
+++ 2-path-params/src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
